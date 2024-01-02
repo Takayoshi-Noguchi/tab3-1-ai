67472caa846161dbb2e03bdeb5a2b9831010f15a
Takayoshi-Noguchi <yoshiyoshi115599@gmail.com>
2024-01-02 20:26:40 +0900

    タブを追加（表示のみ）

--- src/app/aa101-top/aa101-top.component.ts
+++ src/app/aa101-top/aa101-top.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-aa101-top',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class Aa101TopComponent {
 
+  tabs = [
+    {
+      screenUrl: 'aa201g01',
+      name: '契約情報照会'
+    },
+    {
+      screenUrl: 'aa202g01',
+      name: '商品情報'
+    },
+  ];
+  
+  currentScreenUrl = '';
+
+  constructor(private router: Router) {
+  }
+
+  changeTab(screenUrl: string) {
+    this.currentScreenUrl = screenUrl;
+    window.console.log("タブが押された");
+    this.router.navigate([screenUrl])
+  }
 }
