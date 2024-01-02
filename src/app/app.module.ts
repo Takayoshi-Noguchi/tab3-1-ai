816450ae9ce2dc513409236781ded425d1e7fdc5
Takayoshi-Noguchi <yoshiyoshi115599@gmail.com>
2024-01-02 17:44:32 +0900

    top画面まで作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main.component';
+import { MainModule } from './main.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
+    MainModule,
     AppRoutingModule
   ],
   providers: [],
