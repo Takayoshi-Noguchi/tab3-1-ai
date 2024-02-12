861f851c6d930fec6d54ba1143694ac3bee385a6
Takayoshi-Noguchi <yoshiyoshi115599@gmail.com>
2024-02-12 20:52:22 +0900

    Revert "画面保存の対応"
    This reverts commit c322eeed80104328e307eb9ef513e3d435fbe6d6.

--- src/app/aa101-top/aa201g01/aa201g01.component.ts
+++ src/app/aa101-top/aa201g01/aa201g01.component.ts
@@ -1,36 +1,19 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
 
 import { Az101TabService } from '../common/az101-tab/az101-tab.service';
-import { CommonComponent } from '../common/az102-common/az102-common.component';
 
 @Component({
   selector: 'app-aa201g01',
   templateUrl: './aa201g01.component.html',
   styleUrls: ['./aa201g01.component.scss']
 })
-export class Aa201g01Component extends CommonComponent {
+export class Aa201g01Component {
 
-  searchForm = this.fb.group({
-    referNo: ['']
-  })
+
+  constructor(private tabService: Az101TabService) {}
 
   onClickAa202g01() {
     window.console.log('onClickAa202g01()');
     this.tabService.addTab('aa202g01');
   }
-
-  override keep() {
-    let data = {
-      inputForm: this.searchForm,
-    };
-    window.console.log('Aa201g01Component keep()')
-    return data;
-  }
-
-  constructor(
-    private tabService: Az101TabService,
-    private fb: FormBuilder) { 
-      super();
-    }
 }
--- src/app/aa101-top/aa202g01/aa202g01.component.ts
+++ src/app/aa101-top/aa202g01/aa202g01.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
 
-import { CommonComponent } from '../common/az102-common/az102-common.component';
-
 @Component({
   selector: 'app-aa202g01',
   templateUrl: './aa202g01.component.html',
   styleUrls: ['./aa202g01.component.scss']
 })
-export class Aa202g01Component  extends CommonComponent {
+export class Aa202g01Component {
 
-  override keep() {
-    let data = {
-    };
-    window.console.log('Aa202g01Component keep()')
-    return data;
-  }
 }
